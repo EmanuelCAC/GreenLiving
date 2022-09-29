3cf5d57da4cf062aceea3d4743f6df5fe99808d0
EmanuelCAC <emanuelc279@gmail.com>
2022-09-29 11:02:23 -0300

    Placholer para o sobre nós, ajuste dos links e adição de mais uma tirinha

--- sobre_nós.php
+++ sobre_nós.php
@@ -36,18 +36,18 @@
                                 <a class="nav-link" href="./sobre_nós.php">Sobre</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#sustentabilidade">Sustentabilidade</a>
+                                <a class="nav-link" href="./index.php#sustentabilidade">Sustentabilidade</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#tirinhas">Tirinhas</a>
+                                <a class="nav-link" href="./index.php#tirinhas">Tirinhas</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Redes Sociais
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Instagran</a></li>
-                                    <li><a class="dropdown-item" href="#">Email</a></li>
+                                    <li><a class="dropdown-item" href="https://instagram.com/greenliving_pia?igshid=YmMyMTA2M2Y=" target="blank">Instagran</a></li>
+                                    <li><a class="dropdown-item" href="#"> GreenLivingPIA@gmail.com</a></li>
                                 </ul>
                             </li>
                         </ul>
@@ -73,8 +73,14 @@
                                 <div class="card">
                                     <img src="img/..." class="card-img-top">
                                     <div class="card-body">
-                                        <p class="card-text"> - Nome: Bárbara Vitória Conceição Ferreira <br/>
-                                        - Estudante de Técnico em Infortica
+                                        <p class="card-text"> Bárbara Vitória Conceição Ferreira is a computer
+                                            science student at the Federal Institute of Education, Science and
+                                            Technology of São Paulo - Campus Caraguatatuba. She's responsible
+                                            for the entire written part of the Green Living website, she's
+                                            17 years old, likes to study mathematics and explore different areas
+                                            of knowledge. She adopts positivity in her daily life and is a big
+                                            dreamer. Happiness and academic/personal fulfillment are her greatest
+                                            aspirations.
                                         </p>
                                     </div>
                                 </div>
@@ -84,8 +90,15 @@
                                 <div class="card">
                                     <img src="img/..." class="card-img-top">
                                     <div class="card-body">
-                                        <p class="card-text"> - Nome: Emanuel da Costa Andrade Cardoso <br/>
-                                        - Estudante de Técnico em Informática
+                                        <p class="card-text"> Emanuel da Costa Andrade Cardoso is a computer
+                                            science student at the Federal Institute of Education, Science and
+                                            Technology os São Paulo - Caraguatatuba Campus and worked as a
+                                            full-stack programmer in the production of Green Living website. with
+                                            18 years, he's a really calm, rational person and have a deep passion
+                                            about the field of technology. He's also experienced with mathematics,
+                                            having won two OBMEP bronze medals, in addiction he's also intrested
+                                            in various activities such as playing the piano and cello, drawing,
+                                            playing volleyball and video games. 
                                         </p>
                                     </div>
                                 </div>
@@ -95,8 +108,13 @@
                                 <div class="card">
                                     <img src="img/..." class="card-img-top">   
                                     <div class="card-body">
-                                        <p class="card-text"> - Nome: Higor Reis Rodrigues<br/>
-                                        - Estudante de Técnico em Informática</p>
+                                        <p class="card-text"> Higor Reis Rodrigues is a computer science student
+                                            at the Federal Institute of Education, Science and Technology of São
+                                            Paulo. He's responsible for assisting in the visual development
+                                            of the site and in creating the visual identity of the company Green
+                                            Living. At 17 years old, his mission is to make access to knowledge
+                                            of sustainable practices objective and practical. He likes to listen
+                                            to music and enjoy a sunny day with friends.</p>
                                     </div>  
                                 </div>
                             </div>
@@ -116,8 +134,14 @@
                                 <div class="card">
                                     <img src="img/..." class="card-img-top">
                                     <div class="card-body">
-                                        <p class="card-text"> - Nome: Marcelly Santos Paula<br/>
-                                        - Estudante de Técnico em Informática</p>
+                                        <p class="card-text"> Marcelly Santos, like the other team members, is a
+                                            computer science student at the Federal Institute of Education,
+                                            Science and Technology of São Paulo - Campus Caraguatatuba. She's
+                                            responsible for the illustrations, design and administration of the
+                                            social networks of the company Green Living. Marcelly is 17 years
+                                            old, enjoys painting, reading books, practicing her favorite
+                                            instruments and taking care of her pets. She seeks to be an always
+                                            pleasant and a honest person, happy with her choices.</p>
                                     </div>
                                 </div>
                             </div>
@@ -126,8 +150,16 @@
                                 <div class="card">
                                     <img src="img/..." class="card-img-top">   
                                     <div class="card-body">
-                                        <p class="card-text"> - Nome: Victor Hugo Leardini Feliciano<br/>
-                                        - Estudante de Técnico em Informática</p>
+                                        <p class="card-text"> Victor Leardini is a senior computer science student
+                                            at the Federal Institute of Education, Science and Technology of São
+                                            Paulo - Caraguatatuba Campus and works as a designer, supervisor,
+                                            and programming assistant at the company Green Living. He's currently
+                                            17 years old and has great plans for the future, such as helping to 
+                                            change the world, studying psychology, and raising a family that will be
+                                            proud of who he is. He uses his knowledge to help everyone who needs it,
+                                            and intends to continue doing this, becoming more and more someone who
+                                            can be proud of himself. Victor enjoys spending time with his friends,
+                                            animals, especially cats, movies, and playing volleyball.</p>
                                     </div>  
                                 </div>
                             </div>
@@ -200,7 +232,7 @@
                                         </a>
 
                                         <!--Instagram-->
-                                        <a href="#!">
+                                        <a href="https://instagram.com/greenliving_pia?igshid=YmMyMTA2M2Y=" target="blank">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-instagram text-dark" viewBox="0 0 16 16">
                                                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
                                             </svg>
